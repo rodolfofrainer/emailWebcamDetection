4086cd78454be748d76bfdbc1267b8a281061ba3
Rodolfo Frainer <91473305+nellymaw@users.noreply.github.com>
2023-03-10 12:54:27 +0000

    identify when new object appears on video

--- main.py
+++ main.py
@@ -5,9 +5,32 @@ RUNNING = True
 video = cv2.VideoCapture(0)
 time.sleep(1)
 
+first_frame = None
+
 while RUNNING:
     check, frame = video.read()
-    cv2.imshow('My Video', frame)
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray_frame_gau = cv2.GaussianBlur(gray_frame, (11, 11), 0)
+
+    if first_frame is None:
+        first_frame = gray_frame_gau
+
+    delta_frame = cv2.absdiff(first_frame, gray_frame_gau)
+
+    thresh_frame = cv2.threshold(delta_frame, 60, 255, cv2.THRESH_BINARY)[1]
+    dilate_frame = cv2.dilate(thresh_frame, None, iterations=2)
+
+    contours, check = cv2.findContours(
+        dilate_frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    for contour in contours:
+        if cv2.contourArea(contour) < 5000:
+            continue
+
+        x, y, w, h = cv2.boundingRect(contour)
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 3)
+
+    cv2.imshow('Video', frame)
 
     key = cv2.waitKey(1)
 
